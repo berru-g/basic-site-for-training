dfafcf0171685fed2adb42b6632faf8f57703f11
berru-g <g.leberruyer@gmail.com>
2023-04-24 23:15:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# basic site for training
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/h-lautre/pen/eYPBJXN](https://codepen.io/h-lautre/pen/eYPBJXN).
+
---
+++ src/script.js
@@ -0,0 +1,26 @@
+Swal.fire({
+  position: 'bottom-end',
+  icon: 'success',
+  title: 'Mise à jour des offres',
+  showConfirmButton: false,
+  timer: 1500
+})
+
+/***Sweetalert2 librairie****/
+function openBooking(element) {
+  // Récupérer l'URL affiliée à partir de l'attribut data-affiliate-url de la balise parente
+  const affiliateUrl = element.closest('#product').dataset.affiliateUrl;
+  Swal.fire({
+    title: 'Vérifier les dates disponibles.',
+    text: 'Ouvrir le lien dans Booking.com ?',
+    icon: 'success',
+    showCancelButton: true,
+    confirmButtonText: 'Oui',
+    cancelButtonText: 'Non'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.open(affiliateUrl, '_blank');
+    }
+  });
+}
+ 
