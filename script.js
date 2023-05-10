3adf26e994a42a2586ccbd0d67e00ccbdefcc08f
berru-g <g.leberruyer@gmail.com>
2023-05-10 20:17:05 +0200

    lien-future-funnel

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 
-/*Swal.fire({
-  position: 'bottom-end',
-  icon: 'success',
-  title: 'Mise à jour des offres',
-  showConfirmButton: false,
-  timer: 1500
-})
-*/
-Swal.fire({
-  imageUrl: 'https://placeholder.pics/svg/300x1500',
-  imageHeight: 1500,
-  imageAlt: 'A tall image'
-})
-
 /***Sweetalert2 librairie****/
 function openBooking(element) {
   // Récupérer l'URL affiliée à partir de l'attribut data-affiliate-url de la balise parente
